24e4e91cff7251a4942803a217a379217cbb2c1c
masamichiyagi <kurenosono.ai@yahoo.co.jp>
2020-01-27 09:21:28 +0900

    utility fixed

---
+++ utility/pdf2images.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from pdf2image import convert_from_path
+import os
+import sys
+import glob
+import argparse
+
+
+###################################
+## Argument Parser
+###################################
+def arg_parser():
+    parser = argparse.ArgumentParser(description='Filters')
+    parser.add_argument('-i', '--infile',  dest='infile',  help='input file', required=True)
+    parser.add_argument('-e', '--expansion',  dest='expansion',  help='expansion', default='png', required=False)
+    args = parser.parse_args()
+
+    if (not os.path.exists(args.infile)):
+        print ("input file does not exists : " + args.infile)
+        sys.exit(1)
+    return args
+
+
+###################################
+## Main roop
+###################################
+def main():
+    args = arg_parser()
+
+    images = convert_from_path(args.infile, 300) #300=DPI
+    for i in range(len(images)):
+        images[i].save(args.infile + "-{0:03d}".format(i) + "." + args.expansion, args.expansion)
+
+if __name__ == '__main__':
+    main()
