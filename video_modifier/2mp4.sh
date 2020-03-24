c798d7c8821f13aad5f44e50ce898a1309c2a2e8
masamichiyagi <kurenosono.ai@yahoo.co.jp>
2020-03-24 14:53:41 +0900

    video modifier fixed

--- video_modifier/2mp4.sh
+++ video_modifier/2mp4.sh
@@ -6,3 +6,6 @@
 # for power point : -pix_fmt yuv420p     :or: -vf format=yuv420p
 
 ffmpeg -i $1 -movflags faststart -vcodec libx264 -an output.mp4
+
+# url m3u8 to mp4
+ffmpeg -i $1 -movflags faststart -c copy -bsf:a aac_adtstoasc $2
