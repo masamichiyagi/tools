727f23a26719ccc1438a7ee2b892c2239c84cbad
masamichiyagi <kurenosono_ai@yahoo.co.jp>
2024-01-23 17:00:44 +0900

    initial setup command(apt) add

--- shell/apt-get_install.sh
+++ shell/apt-get_install.sh
@@ -1,3 +1,6 @@
+##########################
+# Utility
+##########################
 # zip, unrar
 sudo apt-get update -y
 sudo apt-get install zip -y
@@ -15,34 +18,55 @@ sudo apt-get install nkf -y
 
 # git
 sudo apt-get install git -y
-# git config --global user.name "My name"
+# git config --global user.name "My Name"
 # git config --global user.email "myname@example.com"
+# cp vimrc ~/.vimrc
 
+##########################
+# Script
+##########################
+# perl
+sudo apt-get install perl -y
 # python-pip
 sudo apt-get install python-pip -y
 sudo apt-get install python3-pip -y
 
+##########################
+# Utility
+##########################
 # imagemagick convert
 sudo apt-get install imagemagick -y
 
-# pdf editor
+##########################
+# PDF editor
+##########################
 ## pdfs to pdf
 sudo apt-get install ghostscript -y
 ## pdftotext, etc.
 sudo apt-get install poppler-utils -y
 
-# video, audio editor
-## ffmpeg
-sudo apt-get install ffmpeg -y
+##########################
+# Video and audio editor
+##########################
 ## youtube-dl
 pip install youtube-dl
 pip install -U yt-dlp
+## ffmpeg
+sudo apt-get install ffmpeg -y
+### If you are unable to install ffmpeg,
+### you install ffmpeg from a non-PPT public repository.
+### -E option is proxy.
+#sudo -E add-apt-repository ppa:mc3man/trusty-media
+#sudo apt-get update
+#sudo apt-get install ffmpeg
 
 
+##########################
+# Text editor
 ##########################
 # markdown
-##########################
 sudo apt install npm nodejs-legacy fonts-ipafont-gothic
 sudo npm -g install markdown-pdf
 # usage
 # markdown-pdf document.md
+
