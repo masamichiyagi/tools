0e75b880594cf7b7648f923eae2ea119cba3e19a
masamichiyagi <kurenosono_ai@yahoo.co.jp>
2024-01-23 16:38:49 +0900

    initial setup command(apt) add

---
+++ shell/apt-get_install.sh
@@ -0,0 +1,48 @@
+# zip, unrar
+sudo apt-get update -y
+sudo apt-get install zip -y
+sudo apt-get install unrar -y
+
+# wget
+sudo apt-get install wget -y
+sudo apt-get install curl -y
+
+# zsh
+sudo apt-get install zsh -y
+
+# nkf
+sudo apt-get install nkf -y
+
+# git
+sudo apt-get install git -y
+# git config --global user.name "My name"
+# git config --global user.email "myname@example.com"
+
+# python-pip
+sudo apt-get install python-pip -y
+sudo apt-get install python3-pip -y
+
+# imagemagick convert
+sudo apt-get install imagemagick -y
+
+# pdf editor
+## pdfs to pdf
+sudo apt-get install ghostscript -y
+## pdftotext, etc.
+sudo apt-get install poppler-utils -y
+
+# video, audio editor
+## ffmpeg
+sudo apt-get install ffmpeg -y
+## youtube-dl
+pip install youtube-dl
+pip install -U yt-dlp
+
+
+##########################
+# markdown
+##########################
+sudo apt install npm nodejs-legacy fonts-ipafont-gothic
+sudo npm -g install markdown-pdf
+# usage
+# markdown-pdf document.md
