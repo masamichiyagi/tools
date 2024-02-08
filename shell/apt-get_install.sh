ed76252259f564051d00c0137a99c2a767d8a525
masamichiyagi <kurenosono_ai@yahoo.co.jp>
2024-02-08 10:49:27 +0900

    initial setup tools update

--- shell/apt-get_install.sh
+++ shell/apt-get_install.sh
@@ -23,15 +23,6 @@ sudo apt-get install git -y
 # cp vimrc ~/.vimrc
 
 ##########################
-# Script
-##########################
-# perl
-sudo apt-get install perl -y
-# python-pip
-sudo apt-get install python-pip -y
-sudo apt-get install python3-pip -y
-
-##########################
 # Utility
 ##########################
 # imagemagick convert
@@ -46,11 +37,49 @@ sudo apt-get install ghostscript -y
 sudo apt-get install poppler-utils -y
 
 ##########################
+# Text editor
+##########################
+# markdown
+sudo apt install npm nodejs-legacy fonts-ipafont-gothic
+sudo npm -g install markdown-pdf
+# usage
+# markdown-pdf document.md
+
+##########################
+# Script
+##########################
+# perl
+sudo apt-get install perl -y
+# python-pip
+sudo apt-get install python-pip -y
+sudo apt-get install python3-pip -y
+sudo apt install -y python3.8
+#デフォルトを変更
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+sudo update-alternatives --config python3
+
+##########################
+# Python tools
+##########################
+pip3 install numpy
+pip3 install python-opencv
+pip3 install pdf2image
+pip3 install pdfkit
+pip3 install pandas
+pip3 install bs4
+pip3 install lxml
+pip3 install markdown
+#pip3 install matplotlib
+## PIL
+pip3 install pillow
+
+##########################
 # Video and audio editor
 ##########################
 ## youtube-dl
-pip install youtube-dl
-pip install -U yt-dlp
+# pip install youtube-dl
+pip3 install -U yt-dlp
 ## ffmpeg
 sudo apt-get install ffmpeg -y
 ### If you are unable to install ffmpeg,
@@ -60,13 +89,4 @@ sudo apt-get install ffmpeg -y
 #sudo apt-get update
 #sudo apt-get install ffmpeg
 
-
-##########################
-# Text editor
-##########################
-# markdown
-sudo apt install npm nodejs-legacy fonts-ipafont-gothic
-sudo npm -g install markdown-pdf
-# usage
-# markdown-pdf document.md
 
