ed7210ffb35639f23a19db2e441fbd9647bc9206
masamichiyagi <kurenosono_ai@yahoo.co.jp>
2024-03-04 11:03:30 +0900

    yt-dlp tools update

--- video_modifier/youtube-dl_marge.sh
+++ video_modifier/youtube-dl_marge.sh
@@ -1,19 +1,24 @@
-#TITLE=`youtube-dl -e $1`
 TITLE=`yt-dlp -e $1`
 OUT_FNAME=`echo "$2_${TITLE}.mp4" | sed "s@/@_@g"`
-VIDEO_OPTION=$(yt-dlp $1 -F | grep "137 mp4" | wc -l)
+
+OPTION_RESULT=$(yt-dlp $1 -F)
+VIDEO_OPTION=$(echo $OPTION_RESULT | grep "137 mp4" | wc -l)
 if [ ${VIDEO_OPTION} = 1 ]; then
   VIDEO_OPTION=137
 else
-  VIDEO_OPTION=$(yt-dlp $1 -F | grep "136 mp4" | wc -l)
+  VIDEO_OPTION=$(echo $OPTION_RESULT | grep "136 mp4" | wc -l)
   if [ ${VIDEO_OPTION} = 1 ]; then
     VIDEO_OPTION=136
   else
-    VIDEO_OPTION=135
+    VIDEO_OPTION=$(echo $OPTION_RESULT | grep "135 mp4" | wc -l)
+    if [ ${VIDEO_OPTION} = 1 ]; then
+      VIDEO_OPTION=135
+    else
+      VIDEO_OPTION=134
+    fi
   fi
 fi
-#AUDIO_OPTION=$(youtube-dl $1 -F | grep 22 | wc -l)
-AUDIO_OPTION=$(yt-dlp $1 -F | grep "22 m4a" | wc -l)
+AUDIO_OPTION=$(echo $OPTION_RESULT | grep "22 m4a" | wc -l)
 if [ ${AUDIO_OPTION} = 1 ]; then
   AUDIO_OPTION=22
   WORK2FNAME=work2.mp4
@@ -21,10 +26,30 @@ else
   AUDIO_OPTION=140
   WORK2FNAME=work2.m4a
 fi
+
+
+#VIDEO_OPTION=$(yt-dlp $1 -F | grep "137 mp4" | wc -l)
+#if [ ${VIDEO_OPTION} = 1 ]; then
+#  VIDEO_OPTION=137
+#else
+#  VIDEO_OPTION=$(yt-dlp $1 -F | grep "136 mp4" | wc -l)
+#  if [ ${VIDEO_OPTION} = 1 ]; then
+#    VIDEO_OPTION=136
+#  else
+#    VIDEO_OPTION=135
+#  fi
+#fi
+#AUDIO_OPTION=$(yt-dlp $1 -F | grep "22 m4a" | wc -l)
+#if [ ${AUDIO_OPTION} = 1 ]; then
+#  AUDIO_OPTION=22
+#  WORK2FNAME=work2.mp4
+#else
+#  AUDIO_OPTION=140
+#  WORK2FNAME=work2.m4a
+#fi
 echo "TITLE: $2_${TITLE}.mp4"
 echo "AUDIO OPTION: ${AUDIO_OPTION}"
 echo "VIDEO OPTION: ${VIDEO_OPTION} is downloading."
-#youtube-dl $1 -f $VIDEO_OPTION -o work1.mp4
 yt-dlp $1 -f ${VIDEO_OPTION} -o work1.mp4
 if [ ${AUDIO_OPTION} = 22 ]; then
   WORK2FNAME=work2.mp4
@@ -32,7 +57,6 @@ else
   WORK2FNAME=work2.m4a
 fi
 echo "AUDIO OPTION: ${AUDIO_OPTION}, ${WORK2FNAME} is downloading."
-#youtube-dl $1 -f $AUDIO_OPTION -o ${WORK2FNAME}
 yt-dlp $1 -f ${AUDIO_OPTION} -o ${WORK2FNAME}
 echo "${WORK2FNAME} is downloaded."
 
